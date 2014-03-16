d2db2274b139c5a119f7e63a788d818522dff875
bartek 'basz' szurgot <git@baszerr.eu>
2014-03-16 13:44:40 +0100

    added to manual test as well

--- cpp/type_aware_dispatch_manual/main.cpp
+++ cpp/type_aware_dispatch_manual/main.cpp
@@ -3,6 +3,7 @@
 #include "Msgs.hpp"
 #include "Dispatcher.hpp"
 #include "MyDispatcher.hpp"
+#include "sendAll.hpp"
 
 using namespace std;
 
@@ -12,4 +13,7 @@ int main(void)
   MyDispatcher md;
 
   md.dispatch( serialize(m1) );
+
+  for(unsigned i=0; i<1000; ++i)
+    sendAll<250>(md);
 }
