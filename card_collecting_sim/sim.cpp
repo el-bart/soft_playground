dcb87801e6b53c133ec715af741abb2442234bf9
bartek 'basz' szurgot <git@baszerr.eu>
2023-08-16 21:01:47 +0200

    added multi-player setup

--- card_collecting_sim/sim.cpp
+++ card_collecting_sim/sim.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <random>
+#include <iomanip>
 #include <iostream>
 #include <algorithm>
 #include <boost/lexical_cast.hpp>
@@ -20,14 +21,14 @@ struct SimRes
 };
 
 
-auto hasAllCards(std::vector<unsigned> const& cards)
+auto hasAllCards(std::vector<unsigned> const& cards, unsigned const playersCount)
 {
-  auto const present = [](auto c) { return c > 0u; };
+  auto const present = [playersCount](auto c) { return c >= playersCount; };
   return std::all_of( begin(cards), end(cards), present );
 }
 
 template<typename F>
-auto simulateOne(F&& randCard, unsigned const uniqueCardsCount, unsigned const cardsPerPack, double const packPrice)
+auto simulateOne(F&& randCard, unsigned const playersCount, unsigned const uniqueCardsCount, unsigned const cardsPerPack, double const packPrice)
 {
   SimRes sr{uniqueCardsCount};
   do
@@ -41,7 +42,7 @@ auto simulateOne(F&& randCard, unsigned const uniqueCardsCount, unsigned const c
       ++sr.cards_[pos];
     }
   }
-  while( not hasAllCards(sr.cards_) );
+  while( not hasAllCards(sr.cards_, playersCount) );
   return sr;
 }
 
@@ -79,12 +80,12 @@ struct SimHist
 
 
 template<typename F>
-auto simulate(F&& randCard, unsigned const uniqueCardsCount, unsigned const cardsPerPack, double const packPrice, unsigned const simulations)
+auto simulate(F&& randCard, unsigned const playersCount, unsigned const uniqueCardsCount, unsigned const cardsPerPack, double const packPrice, unsigned const simulations)
 {
   SimHist hist;
   for(auto i = 0u; i < simulations; ++i)
   {
-    auto const res = simulateOne(randCard, uniqueCardsCount, cardsPerPack, packPrice);
+    auto const res = simulateOne(randCard, playersCount, uniqueCardsCount, cardsPerPack, packPrice);
     hist.update(res);
   }
   return hist;
@@ -93,11 +94,18 @@ auto simulate(F&& randCard, unsigned const uniqueCardsCount, unsigned const card
 
 void printCsv(SimHist sh)
 {
-  cout << "iteration, samples, totalPrice, cardsAvg\n";
+  cout << "iteration, samples, samplesIntegral, totalPrice, cardsAvg\n";
   auto iteration = 0u;
+  auto samplesIntegral = 0u;
   for(auto& e: sh.hist_)
   {
-    cout << iteration << ", " << e.samples_ << ", " << e.totalPrice_ << ", " << e.averageCards_ << "\n";
+    samplesIntegral += e.samples_;
+    cout
+      << std::fixed << std::setprecision(2) << iteration << ", "
+      << std::fixed << std::setprecision(2) << e.samples_ << ", "
+      << std::fixed << std::setprecision(2) << samplesIntegral << ", "
+      << std::fixed << std::setprecision(2) << e.totalPrice_ << ", "
+      << std::fixed << std::setprecision(2) << e.averageCards_ << "\n";
     ++iteration;
   }
 }
@@ -105,21 +113,22 @@ void printCsv(SimHist sh)
 
 int main(int argc, char** argv)
 {
-  if( argc != 1 + 4 )
+  if( argc != 1 + 5 )
   {
-    cerr << argv[0] << " <unique_cards_count> <cards_per_pack> <pack_price> <simulations>\n";
+    cerr << argv[0] << " <players_count> <unique_cards_count> <cards_per_pack> <pack_price> <simulations>\n";
     return 2;
   }
 
-  auto const uniqueCardsCount = boost::lexical_cast<unsigned>(argv[1]);
-  auto const cardsPerPack = boost::lexical_cast<unsigned>(argv[2]);
-  auto const packPrice = boost::lexical_cast<double>(argv[3]);
-  auto const simulations = boost::lexical_cast<unsigned>(argv[4]);
+  auto const playersCount = boost::lexical_cast<unsigned>(argv[1]);
+  auto const uniqueCardsCount = boost::lexical_cast<unsigned>(argv[2]);
+  auto const cardsPerPack = boost::lexical_cast<unsigned>(argv[3]);
+  auto const packPrice = boost::lexical_cast<double>(argv[4]);
+  auto const simulations = boost::lexical_cast<unsigned>(argv[5]);
 
   std::mt19937 gen{ std::random_device{}() };
   std::uniform_int_distribution dist{0u, uniqueCardsCount-1u};
   auto randCard = [&] { return dist(gen); };
 
-  auto const simHist = simulate(randCard, uniqueCardsCount, cardsPerPack, packPrice, simulations);
+  auto const simHist = simulate(randCard, playersCount, uniqueCardsCount, cardsPerPack, packPrice, simulations);
   printCsv(simHist);
 }
