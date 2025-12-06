97fa25a12d5ea5a43a6638f2b455d81e71c45c43
bartek 'basz' szurgot <git@baszerr.eu>
2025-12-06 22:19:20 +0100

    unified formatting a bit

--- rp2040/btle/btle_gatt_server.cpp
+++ rp2040/btle/btle_gatt_server.cpp
@@ -45,9 +45,9 @@
  * with a custom GATT Service and a custom Characteristic that sends periodic
  * notifications.
  */
- // *****************************************************************************
+// *****************************************************************************
 
- // System headers
+// System headers
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -105,12 +105,12 @@ static void beat(void);
 #endif
 
 const uint8_t adv_data[] = {
-    // Flags general discoverable
-    0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
-    // Name
-    0x0b, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'L', 'E', ' ', 'C', 'o', 'u', 'n', 't', 'e', 'r',
-    // Incomplete List of 16-bit Service Class UUIDs -- FF10 - only valid for testing!
-    0x03, BLUETOOTH_DATA_TYPE_INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, 0x10, 0xff,
+  // Flags general discoverable
+  0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
+  // Name
+  0x0b, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'L', 'E', ' ', 'C', 'o', 'u', 'n', 't', 'e', 'r',
+  // Incomplete List of 16-bit Service Class UUIDs -- FF10 - only valid for testing!
+  0x03, BLUETOOTH_DATA_TYPE_INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, 0x10, 0xff,
 };
 const uint8_t adv_data_len = sizeof(adv_data);
 
@@ -118,55 +118,55 @@ const uint8_t adv_data_len = sizeof(adv_data);
 static void setup_gatt_service(void){
   printf("setting up GATT service\n");
 
-    l2cap_init();
+  l2cap_init();
 
-    // setup SM: Display only
-    sm_init();
+  // setup SM: Display only
+  sm_init();
 
 #ifdef ENABLE_GATT_OVER_CLASSIC
-    // init SDP, create record for GATT and register with SDP
-    sdp_init();
-    memset(gatt_service_buffer, 0, sizeof(gatt_service_buffer));
-    gatt_create_sdp_record(gatt_service_buffer, sdp_create_service_record_handle(), ATT_SERVICE_GATT_SERVICE_START_HANDLE, ATT_SERVICE_GATT_SERVICE_END_HANDLE);
-    btstack_assert(de_get_len( gatt_service_buffer) <= sizeof(gatt_service_buffer));
-    sdp_register_service(gatt_service_buffer);
+  // init SDP, create record for GATT and register with SDP
+  sdp_init();
+  memset(gatt_service_buffer, 0, sizeof(gatt_service_buffer));
+  gatt_create_sdp_record(gatt_service_buffer, sdp_create_service_record_handle(), ATT_SERVICE_GATT_SERVICE_START_HANDLE, ATT_SERVICE_GATT_SERVICE_END_HANDLE);
+  btstack_assert(de_get_len( gatt_service_buffer) <= sizeof(gatt_service_buffer));
+  sdp_register_service(gatt_service_buffer);
 
-    // configure Classic GAP
-    gap_set_local_name("GATT Counter BR/EDR 00:00:00:00:00:00");
-    gap_ssp_set_io_capability(SSP_IO_CAPABILITY_DISPLAY_YES_NO);
-    gap_discoverable_control(1);
+  // configure Classic GAP
+  gap_set_local_name("GATT Counter BR/EDR 00:00:00:00:00:00");
+  gap_ssp_set_io_capability(SSP_IO_CAPABILITY_DISPLAY_YES_NO);
+  gap_discoverable_control(1);
 #endif
 
-    // setup ATT server
-    att_server_init(profile_data, att_read_callback, att_write_callback);
+  // setup ATT server
+  att_server_init(profile_data, att_read_callback, att_write_callback);
 
-    // setup battery service
-    battery_service_server_init(battery);
+  // setup battery service
+  battery_service_server_init(battery);
 
-    // setup advertisements
-    uint16_t adv_int_min = 0x0030;
-    uint16_t adv_int_max = 0x0030;
-    uint8_t adv_type = 0;
-    bd_addr_t null_addr;
-    memset(null_addr, 0, 6);
-    gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0, null_addr, 0x07, 0x00);
-    gap_advertisements_set_data(adv_data_len, (uint8_t*) adv_data);
-    gap_advertisements_enable(1);
+  // setup advertisements
+  uint16_t adv_int_min = 0x0030;
+  uint16_t adv_int_max = 0x0030;
+  uint8_t adv_type = 0;
+  bd_addr_t null_addr;
+  memset(null_addr, 0, 6);
+  gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0, null_addr, 0x07, 0x00);
+  gap_advertisements_set_data(adv_data_len, (uint8_t*) adv_data);
+  gap_advertisements_enable(1);
 
-    // register for HCI events
-    hci_event_callback_registration.callback = &packet_handler;
-    hci_add_event_handler(&hci_event_callback_registration);
+  // register for HCI events
+  hci_event_callback_registration.callback = &packet_handler;
+  hci_add_event_handler(&hci_event_callback_registration);
 
-    // register for ATT event
-    att_server_register_packet_handler(packet_handler);
+  // register for ATT event
+  att_server_register_packet_handler(packet_handler);
 
-    // set one-shot timer
-    heartbeat.process = &heartbeat_handler;
-    btstack_run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
-    btstack_run_loop_add_timer(&heartbeat);
+  // set one-shot timer
+  heartbeat.process = &heartbeat_handler;
+  btstack_run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
+  btstack_run_loop_add_timer(&heartbeat);
 
-    // beat once
-    beat();
+  // beat once
+  beat();
 }
 /* LISTING_END */
 
@@ -177,32 +177,32 @@ static void setup_gatt_service(void){
  * and request a ATT_EVENT_CAN_SEND_NOW to send a notification if enabled see Listing heartbeat.
  */
 
- /* LISTING_START(heartbeat): Hearbeat Handler */
+/* LISTING_START(heartbeat): Hearbeat Handler */
 static int  counter = 0;
 static char counter_string[30];
 static int  counter_string_len;
 
 static void beat(void){
-    counter++;
-    counter_string_len = snprintf(counter_string, sizeof(counter_string), "BTstack counter %04u", counter);
-    puts(counter_string);
+  counter++;
+  counter_string_len = snprintf(counter_string, sizeof(counter_string), "BTstack counter %04u", counter);
+  puts(counter_string);
 }
 
 static void heartbeat_handler(struct btstack_timer_source *ts){
   beat();
-    if (le_notification_enabled) {
-        att_server_request_can_send_now_event(con_handle);
-    }
+  if (le_notification_enabled) {
+    att_server_request_can_send_now_event(con_handle);
+  }
 
-    // simulate battery drain
-    battery--;
-    if (battery < 50) {
-        battery = 100;
-    }
-    battery_service_server_set_battery_value(battery);
+  // simulate battery drain
+  battery--;
+  if (battery < 50) {
+    battery = 100;
+  }
+  battery_service_server_set_battery_value(battery);
 
-    btstack_run_loop_set_timer(ts, HEARTBEAT_PERIOD_MS);
-    btstack_run_loop_add_timer(ts);
+  btstack_run_loop_set_timer(ts, HEARTBEAT_PERIOD_MS);
+  btstack_run_loop_add_timer(ts);
 }
 /* LISTING_END */
 
@@ -216,32 +216,32 @@ static void heartbeat_handler(struct btstack_timer_source *ts){
 
 /* LISTING_START(packetHandler): Packet Handler */
 static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
-    UNUSED(channel);
-    UNUSED(size);
+  UNUSED(channel);
+  UNUSED(size);
 
-    if (packet_type != HCI_EVENT_PACKET) return;
+  if (packet_type != HCI_EVENT_PACKET) return;
 
-    switch (hci_event_packet_get_type(packet))
-    {
-        case BTSTACK_EVENT_STATE:
-        {
-            bd_addr_t local_addr;
-            if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
-            gap_local_bd_addr(local_addr);
-            printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
-            led_off();
-        }
-        break;
+  switch (hci_event_packet_get_type(packet))
+  {
+    case BTSTACK_EVENT_STATE:
+      {
+        bd_addr_t local_addr;
+        if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
+        gap_local_bd_addr(local_addr);
+        printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
+        led_off();
+      }
+      break;
 
-        case HCI_EVENT_DISCONNECTION_COMPLETE:
-            le_notification_enabled = 0;
-            break;
-        case ATT_EVENT_CAN_SEND_NOW:
-            att_server_notify(con_handle, ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE, (uint8_t*) counter_string, counter_string_len);
-            break;
-        default:
-            break;
-    }
+    case HCI_EVENT_DISCONNECTION_COMPLETE:
+      le_notification_enabled = 0;
+      break;
+    case ATT_EVENT_CAN_SEND_NOW:
+      att_server_notify(con_handle, ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE, (uint8_t*) counter_string, counter_string_len);
+      break;
+    default:
+      break;
+  }
 }
 
 /* LISTING_END */
@@ -262,12 +262,12 @@ static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *pack
 // - if buffer != NULL, copy data and return number bytes copied
 // @param offset defines start of attribute value
 static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size){
-    UNUSED(connection_handle);
+  UNUSED(connection_handle);
 
-    if (att_handle == ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE){
-        return att_read_callback_handle_blob((const uint8_t *)counter_string, counter_string_len, offset, buffer, buffer_size);
-    }
-    return 0;
+  if (att_handle == ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE){
+    return att_read_callback_handle_blob((const uint8_t *)counter_string, counter_string_len, offset, buffer, buffer_size);
+  }
+  return 0;
 }
 /* LISTING_END */
 
@@ -285,19 +285,19 @@ static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t a
 
 /* LISTING_START(attWrite): ATT Write */
 static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t transaction_mode, uint16_t offset, uint8_t *buffer, uint16_t buffer_size){
-    switch (att_handle){
-        case ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_CLIENT_CONFIGURATION_HANDLE:
-            le_notification_enabled = little_endian_read_16(buffer, 0) == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
-            con_handle = connection_handle;
-            break;
-        case ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE:
-            printf("Write: transaction mode %u, offset %u, data (%u bytes): ", transaction_mode, offset, buffer_size);
-            printf_hexdump(buffer, buffer_size);
-            break;
-        default:
-            break;
-    }
-    return 0;
+  switch (att_handle){
+    case ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_CLIENT_CONFIGURATION_HANDLE:
+      le_notification_enabled = little_endian_read_16(buffer, 0) == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
+      con_handle = connection_handle;
+      break;
+    case ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE:
+      printf("Write: transaction mode %u, offset %u, data (%u bytes): ", transaction_mode, offset, buffer_size);
+      printf_hexdump(buffer, buffer_size);
+      break;
+    default:
+      break;
+  }
+  return 0;
 }
 
 /**
@@ -305,25 +305,25 @@ static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_h
  */
 int main()
 {
-    // Setup IO first
-    stdio_init_all();
+  // Setup IO first
+  stdio_init_all();
 
-    // Initialize CYW43 driver architecture (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
-    if (cyw43_arch_init())
-    {
-        printf("failed to initialise cyw43_arch\n");
-        return -1;
-    }
+  // Initialize CYW43 driver architecture (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
+  if (cyw43_arch_init())
+  {
+    printf("failed to initialise cyw43_arch\n");
+    return -1;
+  }
 
-    // Turn on LED while HCI is powering up
-    led_on();
+  // Turn on LED while HCI is powering up
+  led_on();
 
-    // Setup our GATT service
-    setup_gatt_service();
+  // Setup our GATT service
+  setup_gatt_service();
 
-    // Turn on Bluetooth HCI
-	hci_power_control(HCI_POWER_ON);
+  // Turn on Bluetooth HCI
+  hci_power_control(HCI_POWER_ON);
 
-    // Run our Bluetooth app
-    btstack_run_loop_execute();
+  // Run our Bluetooth app
+  btstack_run_loop_execute();
 }
